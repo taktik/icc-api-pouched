1f2632fd860f1834faeb188d655cddeda05e0efb
Denis Hamann <dh@taktik.com>
2019-06-03 15:05:04 +0200

    test begin

--- test/test-basic.ts
+++ test/test-basic.ts
@@ -1,14 +1,23 @@
 import { assert } from 'chai'
 import { iccapipouched } from '../src'
+import { XHR } from 'icc-api/icc-api/api/XHR'
 
 describe('iccapipouched', () => {
 	describe('newIccApiPouched', function() {
 		it('should return an object', function() {
-			// TODO: Q AD
-			// host: svc.backendblallbalbla
-			// headers minimum authorization en basic
-			// lastsync c'est ici
-			const iccApiPouched = iccapipouched.newIccApiPouched()
+			const login = 'abdemo'
+			const password = 'knalou'
+			const host = 'https://backend.svc.icure.cloud/rest/v1/'
+			const b64 = btoa(`${login}:${password}`)
+			const headers: Array<XHR.Header> = {
+				'cache-control': 'no-cache',
+				Authorization: `Basic ${b64}`,
+				'Content-Type': 'application/json',
+				Accept: 'application/json'
+			}
+			const lastSync = 1559563185
+			const databaseName = undefined
+			const iccApiPouched = iccapipouched.newIccApiPouched(host, headers, lastSync, databaseName)
 			assert.equal(typeof iccApiPouched, 'object')
 		})
 	})
